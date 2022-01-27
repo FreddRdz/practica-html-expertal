7053871ae063ddf2278e6ce31543bb0c05e33e23
Alfredo Rodriguez <fredd.073100@gmail.com>
2022-01-27 15:04:07 -0600

    Formularios registro y login

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 const PORT = 3030 || process.env.PORT;
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
-  console.log("El servidor esta corriendo");
+  console.log('El servidor esta corriendo');
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./views/home.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/home.html'));
+});
+
+app.get('/registro', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, './views/login.html'));
 });
